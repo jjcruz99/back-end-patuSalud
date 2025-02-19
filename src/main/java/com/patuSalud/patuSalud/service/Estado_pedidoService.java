b9041a33fd552d278695c97dabac77fc6cb673d4
kathecar <katherinnecardenas16@gmail.com>
2025-02-19 12:14:08 -0500

    Agrego sevice de estado pedido

--- src/main/java/com/patuSalud/patuSalud/service/Estado_pedidoService.java
+++ src/main/java/com/patuSalud/patuSalud/service/Estado_pedidoService.java
@@ -1,4 +1,50 @@
 package com.patuSalud.patuSalud.service;
 
-public class Estado_pedidoService {
+import com.patuSalud.patuSalud.model.Estado_pedido;
+import com.patuSalud.patuSalud.repository.Iestado_pedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Estado_pedidoService implements Iestado_pedidoService{
+    @Autowired
+    private Iestado_pedidoRepository estadoPedidoRepository;
+
+
+    @Override
+    public void guardarEstadoPedido(Estado_pedido estado) {
+        estadoPedidoRepository.save(estado);
+    }
+
+    @Override
+    public void actualizarEstadoPedido(Long id, Estado_pedido pedidoActualizado) {
+        Estado_pedido pedidoExistente = estadoPedidoRepository.findById(id).orElseThrow(() -> new RuntimeException("No se encontró el estado con el ID: " + id));
+
+        if(pedidoExistente != null){
+            pedidoExistente.setDescripcion(pedidoActualizado.getDescripcion());
+
+            //GUardar
+            estadoPedidoRepository.save(pedidoExistente);
+        }
+    }
+
+    @Override
+    public void eliminarEstadoPedido(Long id) {
+        if (estadoPedidoRepository.existsById(id)) {
+            estadoPedidoRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("No se encontró el estado del pedido con el id: " + id);
+        }
+    }
+
+    @Override
+    public Estado_pedido buscarEstadoPedido(Long id) {
+        if (estadoPedidoRepository.existsById(id)) {
+            Estado_pedido pedido = estadoPedidoRepository.findById(id).orElse(null);
+            return pedido;
+        } else {
+            throw new RuntimeException("No se encontró el estado del pedido con el id: " + id);
+        }
+
+    }
 }
